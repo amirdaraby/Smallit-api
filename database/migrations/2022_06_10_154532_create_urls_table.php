425a10edfcadc8bcb0f8cc2336c55e3856992aed
amirdaraby <awmir@outlook.com>
2022-06-10 21:01:12 +0430

    first commit, url model created

---
+++ database/migrations/2022_06_10_154532_create_urls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string("url");
+            $table->string("short_url");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->on("users")->references("id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get("/test",[\App\Http\Controllers\TestController::class,"test"])->name("test");
+Route::get("/show/{url}",[\App\Http\Controllers\TestController::class,"show"])->name("show");
+
+Route::resource("url",\App\Http\Controllers\UrlController::class);
