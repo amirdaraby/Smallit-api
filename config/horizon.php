46bdcd033686f52e6ecdc98c1e2a960eb7f3d8c2
Amirdaraby <amir2002.d@gmail.com>
2023-08-28 12:32:25 +0330

    supervisor and horizon config updated(#30)

--- config/horizon.php
+++ config/horizon.php
@@ -180,7 +180,7 @@ return [
     */
 
     'defaults' => [
-        'supervisor-1' => [
+        'supervisor-clicks' => [
             'connection' => 'redis',
             'queue' => ['default'],
             'balance' => 'auto',
@@ -193,22 +193,44 @@ return [
             'timeout' => 60,
             'nice' => 0,
         ],
+        'supervisor-short-urls' => [
+            'connection' => 'redis',
+            'balance' => 'auto',
+            'autoScalingStrategy' => 'time',
+            'maxProcesses' => 1,
+            'maxTime' => 0,
+            'maxJobs' => 0,
+            'memory' => 128,
+            'tries' => 3,
+            'timeout' => 60,
+            'nice' => 0,
+        ]
     ],
 
     'environments' => [
             'production' => [
-                'supervisor-1' => [
-                    'queue' => ['short-urls', 'clicks'],
+                'supervisor-clicks' => [
+                    'queue' => ['clicks'],
                     'maxProcesses' => 10,
                     'balanceMaxShift' => 1,
                     'balanceCooldown' => 3,
                 ],
+                'supervisor-short-urls' => [
+                    'queue' => ['short-urls'],
+                    'maxProcesses' => 1,
+                    'balanceMaxShift' => 1,
+                    'balanceCooldown' => 3,
+                ]
             ],
 
             'local' => [
-                'supervisor-1' => [
-                    'queue' => ['short-urls', 'clicks'],
+                'supervisor-clicks' => [
+                    'queue' => ['clicks'],
                     'maxProcesses' => 10,
+                ],
+                'supervisor-short-urls' => [
+                    'queue' => ['short-urls'],
+                    'maxProcesses' => 1,
                 ],
             ],
         ],
